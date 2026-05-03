dd6f0989899cba89b7ce5b6a89748568065f1968
Marcos Vinicius <marcosvn1152@gmail.com>
2026-05-03 00:21:25 -0300

    organizando o codigo

--- teste.sql
+++ teste.sql
@@ -1,5 +1,5 @@
+DROP TABLE IF EXISTS pedidos CASCADE;
 DROP TABLE IF EXISTS usuarios CASCADE;
-
 
 CREATE TABLE usuarios(
     id SERIAL PRIMARY KEY,
@@ -14,11 +14,10 @@ CREATE TABLE pedidos(
     preco INT,
     usuario_id INT,
     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
 
-INSERT INTO usuarios (nome, idade)  
+INSERT INTO usuarios (nome, idade)
 VALUES
 ('marcos',22),
 ('henrique',19),
@@ -26,10 +25,6 @@ VALUES
 
 INSERT INTO pedidos (nome, preco, usuario_id)
 VALUES
-('playstation',3000,3),
+('playstation',3000,1),
 ('boneco',40,2),
-('carro',50000,1);
-
-
-
-
+('carro',50000,3);
