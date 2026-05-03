2328d9c50d129c0a762e773226930b3bf99af2a4
Marcos Vinicius <marcosvn1152@gmail.com>
2026-05-02 23:12:08 -0300

    adicionando o join

---
+++ teste.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS usuarios CASCADE;
+
+
+CREATE TABLE usuarios(
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    idade INT,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE pedidos(
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(70) NOT NULL,
+    preco INT,
+    usuario_id INT,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
+
+INSERT INTO usuarios (nome, idade)  
+VALUES
+('marcos',22),
+('henrique',19),
+('jose',44);
+
+INSERT INTO pedidos (nome, preco, usuario_id)
+VALUES
+('playstation',3000,3),
+('boneco',40,2),
+('carro',50000,1);
+
+
+
+
